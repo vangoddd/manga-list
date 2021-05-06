e414611f63a22d9b92b7944916799d4c83f0f6c2
vangoddd <vachri.attala@gmail.com>
2021-05-06 16:13:52 +0700

    cleaning up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+      <h1>Hello world</h1>
     </div>
   );
 }
